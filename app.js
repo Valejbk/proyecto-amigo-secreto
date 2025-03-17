836d348f08fb464f968a1049f6f5d0ca8c08894f
Valejbk <valevalparaiso@gmail.com>
2025-03-17 18:54:05 +0300

    subiendo cambios en funciones

--- app.js
+++ app.js
@@ -1,72 +1,35 @@
-let amigoSecreto = generarSorteo
-let intentos = 1;
-
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return
-}
+let amigoSecreto = [];
 
 function agregarAmigo() {
-    let detalleAmigos = document.getElementById('amigo').value; 
-    console.log(detalleAmigos);
-
-    intentos++;
-
-    return;
+  let amigos = document.getElementById("amigo").value;
+  if (amigos.trim() === "") {
+    alert("Ingresar un nombre válido");
+  } else {
+    amigoSecreto.push(amigos);
+    document.querySelector("#amigo").value = "";
+    mostrarAmigos();
+  }
+  console.log(amigos);
 }
+function mostrarAmigos() {
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
 
-function limpiarCaja() {
-    document.querySelector('#amigo').value = '';
+  for (let index = 0; index < amigoSecreto.length; index++) {
+    const element = amigoSecreto[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+    listaAmigos.appendChild(listaHTML);
+  }
 }
-
-function condicionesIniciales() {
-    asignarTextoElemento('h1', 'Amigo Secreto');
-    asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
-    amigoSecreto = generarSorteo();
-    intentos = 1;
-}
-
 
 function sortearAmigo() {
-    limpiarCaja();
-    condicionesIniciales();
-    document.querySelector('sortear').setAttribute('disabled','true');
+  if (amigoSecreto.length != 0) {
+    let amigoSeleccionado = Math.floor(Math.random() * amigoSecreto.length);
+    console.log(amigoSeleccionado);
+    alert("El ganador es: " + amigoSecreto[amigoSeleccionado]);
+  } else{
+    alert('Debe añadir amigos antes del sorteo');
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-let listaAmigos = [1,2,3,4,5,6];
-let ingresos = 0;
-let maximaCantidadNombres = 6;
-
-
-
-let listaAmigos = prompt("Escribe un nombre: ");
-    /*agregarAmigo()   add*/
-
-    /*agregar un nombre válido*/ 
-
-/*let listaAmigos= [1,2,3,4,5,6]*/
-
-
-/*while {
-   /*agregarAmigo()  add*/
-}
-*/
-
-   /*if (maximaCantidadNombres >6) {
-        alert ('No puedes ingresar más nombres')
-   }*/
-
-/*breack; ¿?*/
